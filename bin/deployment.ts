c93ae19ed9c15c4bb65f5d9f6121ea755a57b95c
nayyaung <nayyaung@gmail.com>
2021-05-09 18:57:03 +0800

    Websocket rest api ECS provisioning

--- bin/deployment.ts
+++ bin/deployment.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { DeploymentStack } from '../lib/deployment-stack';
+import { ApiDeploymentStack } from '../lib/deployment-stack';
 
 const app = new cdk.App();
-new DeploymentStack(app, 'DeploymentStack', {
+new ApiDeploymentStack(app, 'ApiDeploymentStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/deployment-stack.ts
+++ lib/deployment-stack.ts
@@ -1,9 +1,43 @@
 import * as cdk from '@aws-cdk/core';
 
-export class DeploymentStack extends cdk.Stack {
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
+import { join } from 'path';
+
+export class ApiDeploymentStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const image = new DockerImageAsset(this, "WebSocketRestApiImage", {
+      directory: join(__dirname, "../../", "API"),
+    });
+
+    const asImage = new DockerImageAsset(this, "AuthServerImage", {
+      directory: join(__dirname, "../../", "auth"),
+    });
+
+    const vpc = new ec2.Vpc(this, "ApplicationVpc", { maxAzs: 2 });
+
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+    });
+
+    const apiService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "ApiFargateService",
+      {
+        cluster: cluster,
+        cpu: 256,
+        desiredCount: 1,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromDockerImageAsset(image),
+          containerPort: 80,
+        },
+        memoryLimitMiB: 512,
+        publicLoadBalancer: true,
+      }
+    ); 
   }
 }
